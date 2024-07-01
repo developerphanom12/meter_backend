17c1834af21c9cfc8ba17cbdbd52e98698fb7e21
developerphanom12 <147403016+developerphanom12@users.noreply.github.com>
2024-07-01 16:10:54 +0530

    first commit

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -8,7 +8,7 @@ const { validateSeller } = require('../Validation/Validation');
 router.post('/user-registrer', validateSeller, sellercontroller.createseller);
 router.post('/user-login', sellercontroller.loginseller);
 
-
+    
 module.exports = router;
 
 
