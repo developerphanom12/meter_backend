beea70c52bf8a4f10376b40fb2a259be6a6c50c0
developer <developer.phanomprofessionals@gmail.com>
2024-07-26 16:48:59 +0530

    ading

--- Middleware/Multer.js
+++ Middleware/Multer.js
@@ -1,18 +1,45 @@
-const multer =  require("multer")
-const path = require('path');
+<div class="main">
+  <div class="gfg">
+    <p>GeeksForGeeks</p>
+  </div>
+  <div class="custom-wrapper">
+    <div class="header">
+      <h2 class="projtitle">Price Range Slider</h2>
+    </div>
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
-        const extension = path.extname(file.originalname);
-        cb(null, `${uniqueSuffix}${extension}`);
-    },
-});
+    <div class="price-input-container">
+      <div class="price-input">
+        <div class="price-field">
+          <span>Minimum Price</span>
+          <input type="number" class="min-input" value="2500" />
+        </div>
+        <div class="price-field">
+          <span>Maximum Price</span>
+          <input type="number" class="max-input" value="8500" />
+        </div>
+      </div>
+      <div class="slider-container">
+        <div class="price-slider"></div>
+      </div>
+    </div>
 
-const upload = multer({ storage });
-
-
-module.exports = {upload}
+    <div class="range-input">
+      <input
+        type="range"
+        class="min-range"
+        min="0"
+        max="10000"
+        value="2500"
+        step="1"
+      />
+      <input
+        type="range"
+        class="max-range"
+        min="0"
+        max="10000"
+        value="8500"
+        step="1"
+      />
+    </div>
+  </div>
+</div>;
